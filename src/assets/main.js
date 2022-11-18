067e9e04a14fe083f2bd8922ffdf392e0137da15
Adriel-Pezo-2004 <adriefape@gmail.com>
2022-11-18 11:55:40 -0500

    final-web

---
+++ src/assets/main.js
@@ -0,0 +1,44 @@
+
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCvpV1CQ4E4TBf6aPSZ9rICA&part=snippet%2Cid&order=date&maxResults=10';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '46db0c91a4msh775dc5047f77269p109f93jsn65270f4c3b3a',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map(video => `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `).slice(1,6).join('')}
+
+    `;
+    content.innerHTML = view;
+  } catch (error){
+    console.log(error); //en caso que de que haya un error el catch lo captura e imprime qué tipo de error devolvió
+    }
+})();
